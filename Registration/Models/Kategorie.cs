faca53f554cec08ae4cb35370fa744c4957ed014
karoldudek98 <karol.dudek@microsoft.wsei.edu.pl>
2023-10-29 15:14:15 +0100

    Add project files.

---
+++ Registration/Models/Kategorie.cs
@@ -0,0 +1,11 @@
+﻿namespace Registration.Models
+{
+    public enum Kategorie
+    {
+        Low = 1,
+        Normal = 2,
+        High = 3,
+        SuperHigh = 4,
+        UltraHigh = 5
+    }
+}
---
+++ Registration/Models/Registrations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Registration.Models
+{
+    public class Registrations
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        [Phone]
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public string Miasto { get; set; }
+
+    }
+}
