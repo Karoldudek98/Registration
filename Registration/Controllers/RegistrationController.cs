56f32a0ad619a50fecc58704d5d1282f4e8c1149
Karol Dudek <karol.dudek@microsoft.wsei.edu.pl>
2023-11-02 19:03:34 +0100

    Contoroller added

---
+++ Registration/Controllers/RegistrationController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Registration.Models
+
+namespace Registration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: RegistrationController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: RegistrationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: RegistrationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegistrationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistrationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistrationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistrationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: RegistrationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(Registrations registrations)
+        {
+            return View("Wynik", registrations);
+        }
+        public IActionResult Wynik(Registrations registrations)
+        {
+            return View(registrations);
+        }
+    }
+}
--- Registration/Program.cs
+++ Registration/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Registration}/{action=Index}/{id?}");
 
 app.Run();
